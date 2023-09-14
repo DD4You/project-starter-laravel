c05c1fc3e9100f683fd5afb3c8d07c3b0ebdfa83
Vinay Rajput <codewithvinaysingh@gmail.com>
2023-09-14 08:13:38 +0530

    First Commit

---
+++ app/Http/Middleware/Authenticate.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Illuminate\Auth\Middleware\Authenticate as Middleware;
+use Illuminate\Http\Request;
+
+class Authenticate extends Middleware
+{
+    /**
+     * Get the path the user should be redirected to when they are not authenticated.
+     */
+    protected function redirectTo(Request $request): ?string
+    {
+        return $request->expectsJson() ? null : ($request->segment(1) == config('dpanel.prefix') ? route(config('dpanel.prefix') . '.login') : route('login'));
+    }
+}
